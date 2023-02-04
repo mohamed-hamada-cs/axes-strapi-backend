b23b48c2fb62a7bdc7a33539d4a76aa39d8cd87e
Mohamed Hamada <mohamed43628@gmail.com>
2023-02-05 01:53:43 +0200

    adding render.com configs

--- config/database.js
+++ config/database.js
@@ -1,12 +1,6 @@
-const parse = require("pg-connection-string").parse;
-
 const path = require("path");
 
 module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-  }
-
   return {
     connection: {
       client: "sqlite",
