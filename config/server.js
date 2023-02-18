f51b1956e3fb5d719c1547256e59b2ba2db3bbb7
Mohamed Hamada <mohamed43628@gmail.com>
2023-02-18 22:10:45 +0200

    changed the logo

--- config/server.js
+++ config/server.js
@@ -1,7 +1,19 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+
+  // ...
+  admin: {
+    // ...
+    url: "/",
+    // Customize the Strapi admin logo
+    logo: {
+      path: "/assets/images/logo.png",
+      alt: "My Strapi Site",
+    },
+  },
+  // ...
 });
