f53941ef1b90aa947157b30ef584bb96344695ce
root <root@vps.server.local>
2024-01-31 04:24:32 +0000

    overwrite the english titles in the dashboard and add the src/admin/app.js file

---
+++ src/admin/app.js
@@ -0,0 +1,13 @@
+export default {
+    config: {
+        locales: ["en"],
+        translations: {
+            en: {
+                "app.components.LeftMenu.navbrand.title": "Axes Dashboard",
+                "Auth.form.welcome.title": "Welcome to Axes Dashboard!",
+                "Auth.form.welcome.subtitle": "Log in to your Axes Dashboard account"
+            },
+        },
+    },
+    bootstrap() { },
+};
